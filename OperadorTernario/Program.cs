9d5a1f8f338bacb87795b35a2974feb7debd6429
FourDreamsMx <hadilau@hotmail.com>
2017-05-23 21:33:15 -0500

    Commit for the code

---
+++ OperadorTernario/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace OperadorTernario
+{
+    class Program
+    {   
+        private static void Main(string[] args)
+        {
+            string resultado;
+            int valor1 = 10;
+            int valor2 = 6;
+
+            resultado = (valor1 > valor2) ? "Valor verdadero" : "Valor Falso";
+
+            Console.WriteLine(resultado);
+            Console.ReadLine();
+        }
+    }
+}
